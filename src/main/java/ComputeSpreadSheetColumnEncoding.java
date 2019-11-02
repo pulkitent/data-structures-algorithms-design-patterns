3a49ffb37d18ec2c4b5d4a60f3f3f1c1e5496a93
pulkit.gupta <pulkit.gupta@thoughtworks.com>
2019-11-02 21:26:42 +0530

    Add solution for problem where we need to compute column number for excel spreadsheet

---
+++ src/main/java/ComputeSpreadSheetColumnEncoding.java
@@ -0,0 +1,28 @@
+public class ComputeSpreadSheetColumnEncoding {
+    public static void main(String[] args) {
+        //Another Test case
+        //String a = "AA";
+
+        //Another Test case
+        //String a ="ZZ";
+
+        //Another Test case
+        //String a ="AAC";
+
+        String a ="YZ";
+
+        System.out.println(compute(a));
+    }
+
+    private static int compute(String a) {
+        int result = 0;
+        int power = 0;
+
+        for (int index = a.length() - 1; index >= 0; index--) {
+            result = result + (((a.charAt(index) - 'A') + 1) * (int) Math.pow(26, power));
+            power++;
+        }
+
+        return result;
+    }
+}
