65892cba3b760d6e232230bfd5cbd6f726e68657
pulkit.gupta <pulkit.gupta@thoughtworks.com>
2020-01-17 10:52:54 +0530

    Add solution to problem where we need to form largest number from a given integer array

---
+++ src/main/java/com/pulkit/datastructures/algorithms/done/arrays/LargestNumber.java
@@ -0,0 +1,34 @@
+package com.pulkit.datastructures.algorithms.done.arrays;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static java.lang.System.*;
+
+public class LargestNumber implements Comparator<Integer> {
+    public static void main(String[] args) {
+        Integer[] array = {3, 30, 34, 5, 9};
+        out.println(formLargestNumberFrom(array));
+    }
+
+    private static String formLargestNumberFrom(Integer array[]) {
+        LargestNumber largestNumber = new LargestNumber();
+        Arrays.sort(array, largestNumber);
+
+        StringBuilder result = new StringBuilder();
+
+        for (Integer element : array) {
+            result.append(element.toString());
+        }
+
+        return result.toString();
+    }
+
+    @Override
+    public int compare(Integer firstElement, Integer secondElement) {
+        String firstSecondElement = firstElement.toString() + secondElement.toString();
+        String secondFirstElement = secondElement.toString() + firstElement.toString();
+
+        return secondFirstElement.compareTo(firstSecondElement);
+    }
+}
