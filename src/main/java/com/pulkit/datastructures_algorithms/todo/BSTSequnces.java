af3a5f567e94c113d3aee6bf99e31e73efeba18b
Pulkit Gupta-GGN <Pulkit_Gupta-GGN@external.mckinsey.com>
2020-03-10 16:35:49 +0530

    Add problems which needs to be implemented

---
+++ src/main/java/com/pulkit/datastructures_algorithms/todo/BSTSequnces.java
@@ -0,0 +1,10 @@
+package com.pulkit.datastructures_algorithms.todo;
+
+/*
+ A binary search tree was created by traversing through an array from
+ left to right and inserting each element. Given a binary search tree
+ with distinct elements,print all possible arrays that could have led to this tree
+*/
+
+public class BSTSequnces {
+}
---
+++ src/main/java/com/pulkit/datastructures_algorithms/todo/FindInorderSuccessorWithParentPointer.java
@@ -0,0 +1,11 @@
+package com.pulkit.datastructures_algorithms.todo;
+
+/*
+Write an algorithm to find the "next" node (i.e., in-order successor)
+of a given node in a binary search tree.
+You may assume that each node has a link to its parent.
+*/
+
+public class FindInorderSuccessorWithParentPointer {
+
+}
